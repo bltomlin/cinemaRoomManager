3807e356f281f73f2c7064ae775eeedd37ce8ba7
bltomlin <62484274+bltomlin@users.noreply.github.com>
2022-09-01 22:47:40 -0700

    modified math logic to calculate total profit if sold out

--- README.md
+++ README.md
@@ -9,6 +9,52 @@
 #### Run the program
 2. `java manager.java`
 
-The output should be like in the example below and should contain 9 lines.
+##### The program will prompt the user for two positive integer numbers: the number of rows and the number of seats in each row. The ticket price is determined by the following rules:
+
+-If the total number of seats in the screen room is not more than 60, then the price of each ticket is 10 dollars.
+-In a larger room, the tickets are 10 dollars for the front half of the rows and 8 dollars for the back half. Please note that the number of rows can be odd, for example, 9 rows. In this case, the first half is the first 4 rows, and the second half is the other 5 rows.
+
 <br>
-![output](output.jpeg)
+The greater-than symbol followed by a space (> ) represents the user input. Note that it's not part of the input.
+<br>
+`Example 1`
+<br>
+`Enter the number of rows:`
+<br>
+`> 4`
+<br>
+`Enter the number of seats in each row:`
+<br>
+`> 5`
+<br>
+`Total income:`
+<br>
+`$200`
+<br>
+Example 2
+<br>
+`Enter the number of rows:`
+<br>
+`> 8`
+<br>
+`Enter the number of seats in each row:`
+<br>
+`> 9`
+<br>
+`Total income:`
+<br>
+`$648`
+<br>
+Example 3
+<br>
+`Enter the number of rows:`
+<br>
+`> 9`
+<br>
+`Enter the number of seats in each row:`
+<br>
+`> 7`
+<br>
+`Total income:`
+<br>
+`$560`
--- manager.java
+++ manager.java
@@ -1,15 +1,34 @@
 package cinema;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class Cinema {
 
     public static void main(String[] args) {
-        
-        System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for(int i = 1; i <= 7; i++) {
-            String stringInt = String.valueOf(i);
-            String allS = " S S S S S S S S";
-            System.out.println(stringInt + allS);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int numOfRows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int numOfSeats = scanner.nextInt();
+        int totalSeats = numOfRows * numOfSeats;
+        System.out.println("Total income:");
+        if (totalSeats < 60) {
+            int profit = totalSeats * 10;
+            String printProfit = "$" + String.valueOf(profit);
+            System.out.println(printProfit);
+        } else {
+            if (numOfRows % 2 == 0) {
+                numOfSeats= (numOfRows / 2) * numOfSeats;
+                int profit = (numOfSeats * 8) + (numOfSeats * 10);
+                String printProfit = "$" + String.valueOf(profit);
+                System.out.println(printProfit);
+            } else {
+                int numOfBackRowSeats = ((int)Math.ceil(numOfRows / 2) + 1) * numOfSeats;
+                int numOfFrontRowSeats = ((int)Math.floor(numOfRows / 2)) * numOfSeats;
+                int profit = (numOfFrontRowSeats * 10) + (numOfBackRowSeats * 8);
+                String printProfit = "$" + String.valueOf(profit);
+                System.out.println(printProfit);
+            }
         }
     }
 }
